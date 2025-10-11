aece75f7c0b4b022d5c2f8b90753f38826873b45
Jean Rabello <jean.rabello.prog@gmail.com>
2025-10-10 21:13:13 -0300

    chore: convert jest config file to typescript

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@lang/(.*)$': '<rootDir>/src/lang/$1',
+    '^@middlewares/(.*)$': '<rootDir>/src/shared/middlewares/$1',
+    '^@plugins/(.*)$': '<rootDir>/src/plugins/$1',
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^@utils/(.*)$': '<rootDir>/src/shared/utils/$1',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.(ts|tsx|js)', '**/*.(test|spec).(ts|tsx|js)'],
+  maxWorkers: 1,
+};
+
+export default config;
